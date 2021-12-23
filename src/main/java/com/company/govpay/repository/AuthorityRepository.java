099d7a8d8a2868c76780a75c7a469c58072a1391
enoch-han <62514237+enoch-han@users.noreply.github.com>
2021-12-23 07:25:03 -0800

    Initial version of govpay generated by generator-jhipster@7.3.1

---
+++ src/main/java/com/company/govpay/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.company.govpay.repository;
+
+import com.company.govpay.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
---
+++ src/main/java/com/company/govpay/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.company.govpay.service.dto;
